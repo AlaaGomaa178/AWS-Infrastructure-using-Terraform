fe409f00739b54cc46e25c9eb56c855ff12e3bd7
Alaa Mohamed Gomaa <98563808+AlaaGomaa178@users.noreply.github.com>
2024-05-20 04:54:27 +0300

    Delete config directory

--- config/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-tf-backend-bucket"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-    dynamodb_table = "TF-State-Lock-Dynamo-DB"
-  }
-}
