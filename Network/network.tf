940a6f9dc22d120f0e131598bd4c0ec72bf2b580
Alaa Mohamed Gomaa <98563808+AlaaGomaa178@users.noreply.github.com>
2024-05-20 03:43:59 +0300

    Delete Network directory

--- Network/network.tf
+++
@@ -1,7 +0,0 @@
-module "network" {
-  source               = "./network"
-  vpc_cidr             = var.custom_vpc_cidr
-  common_resource_name = var.custom_common_resource_name
-  region               = var.custom_region
-  subnets_details      = var.custom_subnets_details
-}
